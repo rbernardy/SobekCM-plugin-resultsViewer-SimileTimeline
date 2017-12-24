3f582d3368aed51561658a72fcd81f1470451399
rbernardy <rbernard@usf.edu>
2017-12-24 15:39:35 -0500

    Made corrections - including renaming plugin folder to match viewer code.

--- plugin_folder/Timeline/install-script/SimileTimeline_ResultsViewer-install_script.sql
+++ plugin_folder/Timeline/install-script/SimileTimeline_ResultsViewer-install_script.sql
@@ -1,9 +1,9 @@
 -- Add the TIMELINE view as a viewer type
-if ( not exists ( select 1 from SobekCM_Item_Aggregation_Result_Types where ResultType = 'SIMILETIMELINE' ))
+if ( not exists ( select 1 from SobekCM_Item_Aggregation_Result_Types where ResultType = 'TIMELINE' ))
 begin
 	
 	insert into SobekCM_Item_Aggregation_Result_Types ( ResultType, DefaultOrder, DefaultView )
-	values ( 'SIMILETIMELINE', 15, 'true' );
+	values ( 'TIMELINE', 10, 'true' );
 
 end;
 GO
