c7a44f665d11337317f0869d303cf9c0bde71d7b
Richard Bernardy <rbernard@usf.edu>
2018-07-13 08:09:48 -0400

    Update SimileTimeline_SobekCM_Settings-insert_script.sql
    Correct several typos in script.

--- plugin_folder/Timeline/install-script/SimileTimeline_SobekCM_Settings-insert_script.sql
+++ plugin_folder/Timeline/install-script/SimileTimeline_SobekCM_Settings-insert_script.sql
@@ -1,7 +1,7 @@
 -- Add the TIMELINE SYSTEM WIDE SETTINGS
 if ( not exists ( select 1 from SobekCM_Settings where Setting_Key='Use Timeline Bundle' ))
 begin
-	insert into SobekCM_Setting ( Setting_Key,Setting_Value,TabPage,Heading,Hidden,Reserved,Help,Options,Dimensions )
-	values ( 'Use Timeline Bundle',false,'System / Server Settings','Timeline',0,0,'If true the Timeline JS bundle will be used, if false the source JS will be used','true|false',null );
+	insert into SobekCM_Settings ( Setting_Key,Setting_Value,TabPage,Heading,Hidden,Reserved,Help,Options,Dimensions )
+	values ( 'Use Timeline Bundle','false','System / Server Settings','Timeline',0,0,'If true the Timeline JS bundle will be used, if false the source JS will be used','true|false',null );
 end;
 GO
